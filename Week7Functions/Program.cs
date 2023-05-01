787139dc9f06e8202f8409fb82861d94d9cca24b
Aleksandr Sukonko <aleksandr.sukonko@gmail.com>
2023-05-01 20:52:59 +0300

    version 1

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Are you coming or living? (in/out):");
+string userChois = Console.ReadLine();
+
+if (userChois == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+static void PrintHello()    //function/method   
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()  //void - peale func end mälus mitte midagi ei jää
+{
+    Console.WriteLine("See you later, alligator.");
+}
